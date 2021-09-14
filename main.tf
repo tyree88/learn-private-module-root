512b6fd35d90eb967e21ee55fb129ddf9e9bde8a
tyree <tyreepearson88@gmail.com>
2021-09-14 13:12:38 -0500

    added a t2 micro instance with no tags (should fail from sentinel)

--- main.tf
+++ main.tf
@@ -24,3 +24,28 @@ module "s3-webapp" {
   prefix = var.prefix
   version = "1.0.0"
 }
+
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
+
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+
+#   owners = ["099720109477"] # Canonical
+# }
+
+resource "aws_instance" "web" {
+  # ami           = data.aws_ami.ubuntu.id
+  ami = "ami-029c64b3c205e6cce"
+  instance_type = "t2.micro"
+
+  tags = {
+  }
+}
