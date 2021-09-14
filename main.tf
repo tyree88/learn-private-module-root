74ad2602a1e25cd02ad77603cdeaa8018622c230
tyree <tyreepearson88@gmail.com>
2021-09-14 12:45:45 -0500

    apply to initate git flow for vcs and tf workspace learn-module-root

--- main.tf
+++ main.tf
@@ -1,10 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "tyreepearson"
+    workspaces {
+      name="learn-private-module-root"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name        = var.name
+  source  = "app.terraform.io/tyreepearson/s3-webapp/aws"
+  name   = var.name
   region = var.region
   prefix = var.prefix
   version = "1.0.0"
