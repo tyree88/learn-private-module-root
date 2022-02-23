cb7086d9210fa766f1e4a46835f05dae119941cd
tyree <tyreepearson88@gmail.com>
2022-02-23 12:39:22 -0600

    added the s3 web app module 1.0

--- main.tf
+++ main.tf
@@ -4,48 +4,25 @@ terraform {
       source = "hashicorp/aws"
     }
   }
-  cloud {
-    organization = "tyreepearson"
-    workspaces {
-      name="learn-private-module-root"
-    }
-  }
+  
 }
 
 provider "aws" {
   region = var.region
 }
 
+module "module" {
+  source  = "app.terraform.io/tyreepearson/module/aws"
+  name   = var.name
+  region = var.region
+  prefix = var.prefix
+  version = "1.0.0"
+}
 module "s3-webapp" {
   source  = "app.terraform.io/tyreepearson/s3-webapp/aws"
   name   = var.name
   region = var.region
   prefix = var.prefix
   version = "1.0.0"
-
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  
-  instance_type = "t2.micro"
-  tags = {
-    "Name" = "tyree-pearson"
-  }
+  # insert required variables here
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "website_endpoint" {
-  value = module.s3-webapp.endpoint
+  value = module.module.endpoint
 }
